d325f270c216c84849dbc3c305a622ffec818044
Malak Tayeh <malaktayeh22@gmail.com>
2018-10-11 22:05:00 -0400

    fix: added api key

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -4,7 +4,7 @@ import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-map
 
 const MyMapComponent = compose(
   withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+        googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyDh8QgxRUXxMpGT2LoeuyIdUCmjU5Dhr7w",
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `100vh` }} />,
     mapElement: <div style={{ height: `100%` }} />,
