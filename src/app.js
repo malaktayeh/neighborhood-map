1f6e86e993752a0bd7435547740b2fc703a1c82b
Malak Tayeh <malaktayeh@outlook.com>
2020-02-08 17:26:34 -0500

    Create simple react app w/ parcel 2
    deleted everything and started a new app w/ parcel 2 for simplicity
    of this app.

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import ReactDom from 'react-dom';
+import React from 'react';
+
+function App() {
+  return(
+    <div>
+      <h2>TEST</h2>
+    </div>
+  )
+}
+
+ReactDom.render(<App />, document.getElementById('app'));
