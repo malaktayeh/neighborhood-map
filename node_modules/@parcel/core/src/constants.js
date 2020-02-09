c26dfc3636f9dfd300be0e9fa534e093d0866522
Malak Tayeh <malaktayeh@outlook.com>
2020-02-08 20:46:12 -0500

    add full screen google maps

--- node_modules/@parcel/core/src/constants.js
+++
@@ -1,6 +0,0 @@
-// @flow strict-local
-
-// $FlowFixMe
-import {version} from '../package.json';
-
-export const PARCEL_VERSION = version;
--- src/app.js
+++ src/app.js
@@ -1,10 +1,24 @@
 import ReactDom from 'react-dom';
 import React from 'react';
+import Map from './components/GoogleMap'
 
 function App() {
-  return(
-    <div>
-      <h2>TEST</h2>
+  return (
+    <div id="main">
+      <Map
+        id="myMap"
+        options={{
+          center: { lat: 41.0082, lng: 28.9784 },
+          zoom: 8
+        }}
+        onMapLoad={map => {
+          var marker = new window.google.maps.Marker({
+            position: { lat: 41.0082, lng: 28.9784 },
+            map: map,
+            title: 'Hello Istanbul!'
+          });
+        }}
+      />
     </div>
   )
 }
